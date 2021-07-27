9f98bb6f47661335434990d90a56d367a98b9e6e
Umair Ali <umairmamoor@gmail.com>
2021-07-27 10:11:25 -0400

    user-repo

---
+++ assets/js/homepage.js
@@ -0,0 +1,14 @@
+var getUserRepos = function(user) {
+// format the github api url
+var apiUrl ="https://api.github.com/users/" + user + "/repos";
+
+// make a request to the URL
+
+fetch(apiUrl).then(function(response) {
+    response.json().then(function(data){
+        console.log(data);
+    });
+});
+
+};
+getUserRepos("microsoft");
